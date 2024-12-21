0d77bf1db38e46ea65cced0daaf072f052f3d36a
amaljith18 <amaljithpm18@gmail.com>
2024-12-22 00:54:32 +0530

    initial setup completed

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import {} from 'react'
+import './App.css'
+import Signin from './components/Signin/signin'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Signin />
+        
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Signin/signin.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const signin = () => {
+  return (
+    <div className='grid grid-cols-2 bg-white h-screen' >
+      <div >
+
+      </div>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default signin
